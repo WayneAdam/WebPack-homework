b6fdac1d0a507b8580d24997ba92e6a8e30062bb
Wayne <meibeat@mail.ru>
2023-10-07 16:38:21 +0300

    Домашнее задание WebPack выполнено

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ]
+}
